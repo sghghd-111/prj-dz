d673d94a41cc30d30cffffae57df8ffbfc619ce5
laoding <sghghd@21cn.com>
2020-04-13 22:02:01 +0800

    修改标识名称为ppc9404

--- install_dir/shell/cms-flag-file-check.sh
+++ install_dir/shell/cms-flag-file-check.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 PARAM_PATH="/opt/param/"
-CMS_FLAG_FILE="wddhxws"
+CMS_FLAG_FILE="ppc9404"
 
 if [ ! -f $PARAM_PATH$CMS_FLAG_FILE ];then
 	echo "$PARAM_PATH$CMS_FLAG_FILE not exist, touch it..."
